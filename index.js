23de89e69735d912f9f801018050cd57536059c7
Waleed <qadiwaleed@gmail.com>
2016-11-22 10:59:36 +0200

    fixed store race condition that happend with passportjs

--- index.js
+++ index.js
@@ -40,10 +40,11 @@ module.exports = function (session) {
   RethinkStore.prototype.get = function (sid, fn) {
     var sdata = cache.get('sess-'+sid);
     if (sdata) {
-      if( this.debug ){ console.log( 'SESSION: (get)', JSON.parse(sdata.session) ) };
+      if( this.debug ){ console.log( 'SESSION: (cache get)', JSON.parse(sdata.session) ) };
       return fn(null, JSON.parse(sdata.session));
     } else {
         r.table(this.table).get(sid).run().then(function (data) {
+          if( this.debug ){ console.log( 'SESSION: (get)', JSON.parse(data.session) ) };
           return fn(null, data ? JSON.parse(data.session) : null);
         }).error(function (err) {
           return fn(err);
@@ -53,19 +54,21 @@ module.exports = function (session) {
 
   // Set Session
   RethinkStore.prototype.set = function (sid, sess, fn) {
+    var debug = this.debug;
     var sessionToStore = {
       id: sid,
       expires: new Date().getTime() + (sess.cookie.originalMaxAge || this.sessionTimeout),
       session: JSON.stringify(sess)
     };
-
+    cache.put( 'sess-' + sessionToStore.id, sessionToStore, 30000 );
+    if (this.debug){ console.log( 'SESSION: (set)', sessionToStore ); }
     r.table(this.table).insert(sessionToStore, { conflict: 'replace', returnChanges: true }).run().then(function (data) {
       var sdata = null;
       if(data.changes[0] != null)
         sdata = data.changes[0].new_val || null;
 
+      if (debug){ console.log( 'SESSION: (setted)', sdata.id ); }
       if (sdata){
-          if (this.debug){ console.log( 'SESSION: (set)', sdata.id ); }
           cache.put( 'sess-'+ sdata.id, sdata, 30000 );
       }
       if (typeof fn === 'function') {
@@ -74,6 +77,8 @@ module.exports = function (session) {
       else
         return null
     }).error(function (err) {
+      if (debug){ console.log( 'SESSION: (errored)', err ); }
+      cache.del('sess-' + sessionToStore.id);
       return fn(err);
     });
   };
